6992ba222d3b425a221682eb501f030efd933a2b
Dainius Luzan <dainiusluzan@gmail.com>
2021-03-25 19:47:21 +0200

    points added. WIP

--- targets/targets.ino
+++ targets/targets.ino
@@ -1,6 +1,6 @@
 class Led {
-  int pin;
   public:
+  int pin;
     void construct(int ledPin) {
       pin = ledPin;
       pinMode(pin, INPUT);
@@ -14,10 +14,10 @@ class Target {
   Led redLed;
   Led blueLed;
   int targetPin;
-  int hitTime = 1500;
+  int hitTime = 3000;
   int active = 0;
   long startTime;
-
+  int points[3] = {0, 0, 0};
 
   Target(int target, int red, int blue) {
     redLed.construct(red);
@@ -25,6 +25,7 @@ class Target {
     targetPin = target;
     pinMode(targetPin, INPUT);
   }
+
   void activate(int color) {
     active = color;
     turnOn(color);
@@ -32,18 +33,22 @@ class Target {
   }
 
   bool update() {
-
-    if (millis() - startTime > hitTime) {
-      //Serial.print("Update ");Serial.print(targetPin); Serial.print("\n");
-      //Serial.print("Start time ");Serial.print(startTime); Serial.print("\n");
+    if (millis() - startTime > hitTime || hit()) {
       active = 0;
-      blueLed.off();
-      redLed.off();
+      turnOff();
     }
   }
+
+  bool hit() {
+    if (random(1,10) == 1) {
+      points[active]++;
+      return true;
+    }
+    return false;
+    //return digitalRead(targetPin) == 1;
+  }
+
   void turnOn(int color) {
-    Serial.print(". PIN ");Serial.print(targetPin); Serial.print("\n");
-    Serial.print(". LED ON COLOR ");Serial.print(color); Serial.print("\n");
     if (color == 1) {
       redLed.on();
     } else if (color == 2) {
@@ -51,65 +56,62 @@ class Target {
     }
   }
 
-  void turnOff(int color) {
-    Serial.print(". PIN ");Serial.print(targetPin); Serial.print("\n");
-    Serial.print(". LED OFF COLOR ");Serial.print(color); Serial.print("\n");
-    if (color == 1) {
-      redLed.off();
-    } else if (color == 2) {
-      blueLed.off();
-    }
+  void turnOff() {
+    redLed.off();
+    blueLed.off();
+  }
+
+  int activeColor() {
+    return active;
   }
 };
-const int OFF = 0;
+
 const int RED = 1;
 const int BLUE = 2;
 
-Target target_1(13, 2, 3);
-Target target_2(13, 5, 6);
-Target target_3(13, 8, 9);
-//Target target_4(13, 10, 11);
+Target target_1(1, 5, 6);
+Target target_2(2, 7, 8);
+Target target_3(3, 9, 10);
+Target target_4(4, 11, 12);
+Target targets[] = {target_1, target_2, target_3, target_4};
 
-Target targetsv2[] = {target_1, target_2, target_3};
+int redPoints = 0;
+int bluePoints = 0;
+
 void setup() {
   Serial.begin(9600);
-  randomSeed(analogRead(0));
 }
 
 void loop() {
-  Serial.print(" Loop ");Serial.print("\n");
-  target_1.update();
-  target_2.update();
-  target_3.update();
+  Serial.print("LOOP ");Serial.print(targets[0].active);Serial.print(targets[1].active);Serial.print(targets[2].active);Serial.print(targets[3].active);
+  Serial.print("     RED POINTS ");Serial.print(redPoints);Serial.print(" BLUE POINTS ");Serial.print(bluePoints);
+  Serial.print("\n");
+  for (int i = 0; i++, i < 4;) {
+    targets[i].update();
+    redPoints += targets[i].points[RED];
+    bluePoints += targets[i].points[BLUE];
+  }
+  
   if (isAnyTargetActive(RED) == false) {
     activateRandom(RED);
   }
   if (isAnyTargetActive(BLUE) == false) {
     activateRandom(BLUE);
   }
-  delay(300);
+  delay(100);
 }
 
 bool isAnyTargetActive(int color) {
-  return (target_1.active == color || target_2.active == color|| target_3.active == color);
+  return (targets[0].active == color || targets[1].active == color || targets[2].active == color || targets[3].active == color);
 }
 
 void activateRandom(int color) {
   bool found = false;
   while (found == false) {
-    int targetNr = random(0,3);
-    target_1.update();
-    target_2.update();
-    target_3.update();
-    //Serial.print("Target nr ");Serial.print(targetNr);
-    //Serial.print(". Active ");Serial.print(targetsv2[targetNr].active);
-    //Serial.print(".");
-    
-    if (targetsv2[targetNr].active == 0) {
-      targetsv2[targetNr].activate(color);
+    int targetNr = random(0,4);   
+    if (targets[targetNr].active == 0) {
+      targets[targetNr].activate(color);
       found = true;
     }
   }
-  Serial.print(" Out of while ");Serial.print("\n");
 }
-
